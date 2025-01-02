d2f0a1d7aa850c10383310abaa8435517d9eac43
1Omon <1serwise@gmail.com>
2025-01-02 23:01:33 +0000

    added applicant schema

--- src/db/Applicant.ts
+++ src/db/Applicant.ts
@@ -1,28 +1,72 @@
 import mongoose, { Model } from "mongoose";
 
 const applicantSchema = new mongoose.Schema({
-    backgroundData: {
-        surname: {type: String, required: true},
-        otherNames: {type: String, required: true},
-        dateOfBirth: {type: Date, required: true},
-        nationality: {type: String, required: true},
-        placeOfResidence: {type: String, required: true},
-        digitalAddress: {type: String, required: true},
-        email: {type: String, required: true},
-        contact: {type: String, required: true},
+  backgroundData: {
+    surname: { type: String, required: true },
+    otherNames: { type: String, required: true },
+    dateOfBirth: { type: Date, required: true },
+    nationality: { type: String, required: true },
+    placeOfResidence: { type: String, required: true },
+    digitalAddress: { type: String, required: true },
+    email: { type: String, required: true },
+    contact: { type: String, required: true },
+  },
+  caretakr: {
+    mother: {
+      name: { type: String, required: true },
+      isAlive: { type: Boolean, required: true },
+      contact: { type: String, required: true },
+      address: { type: String, required: true },
+      occupation: { type: String, required: true },
+      nationality: { type: String, required: true },
+    },
+    father: {
+      name: { type: String, required: true },
+      isAlive: { type: Boolean, required: true },
+      contact: { type: String, required: true },
+      address: { type: String, required: true },
+      occupation: { type: String, required: true },
+      nationality: { type: String, required: true },
     },
     guardian: {
-
+      name: { type: String, required: true },
+      isAlive: { type: Boolean, required: true },
+      contact: { type: String, required: true },
+      address: { type: String, required: true },
+      occupation: { type: String, required: true },
+      nationality: { type: String, required: true },
     },
-    authentication: {
-        password: {type: String, required: true, select: false},
-        salt: {type: String, select: false},
-        sessionToken: {type: String, select: false}
+  },
+  authentication: {
+    password: { type: String, required: true, select: false },
+    salt: { type: String, select: false },
+    sessionToken: { type: String, select: false },
+  },
+  academicHistory: {
+    indexNumber: { type: Number, required: true },
+    school: { type: String, required: true },
+    year: { type: Number, required: true },
+    course: { type: String, required: true },
+    examsType: { type: String, required: true },
+    results: [
+      {
+        subject: { type: String },
+        score: { type: Number },
+      },
+    ],
+  },
+  academicAspiration: [
+    {
+      programme: String,
+      priority: Number,
     },
-    academic: {
-        
-    }
-})
+  ],
+  documents: {
+    passportPicture: String,
+    birthCertificate: String,
+    nationalID: String
+}
+});
 
-const Applicant = new Model(applicantSchema, "Applicant")
-export default Applicant
+const Applicant = new Model(applicantSchema, "Applicant");
+export default Applicant;
