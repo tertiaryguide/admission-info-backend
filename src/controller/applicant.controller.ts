8084ced70f3328935196258a16ead002e4b7b02e
1Omon <1serwise@gmail.com>
2025-01-03 23:47:42 +0000

    started writing controller codes

---
+++ src/controller/applicant.controller.ts
@@ -0,0 +1,28 @@
+import express from "express";
+
+export const newData = async (req: express.Request, res: express.Response) => {
+  try {
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(500)
+  }
+};
+
+
+export const editData = async (req: express.Request, res: express.Response) => {
+    try {
+    } catch (error) {
+      console.log(error)
+      res.sendStatus(500)
+    }
+  };
+
+  
+  export const deleteData = async (req: express.Request, res: express.Response) => {
+    try {
+    } catch (error) {
+      console.log(error)
+      res.sendStatus(500)
+    }
+  };
+  
