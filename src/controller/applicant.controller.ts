e90b9909778735d46492122ef5fb18fa56514eba
1Omon <1serwise@gmail.com>
2025-01-07 02:49:10 +0000

    wrote newData controller

--- src/controller/applicant.controller.ts
+++ src/controller/applicant.controller.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import { ApplicantModel } from "models/Applicant";
 
 export const newData = async (req: express.Request, res: express.Response) => {
   try {
+    const {fname, lname, oname, dob, gender, nationality, religion, email, contact} = req.body
+    const newD = await ApplicantModel.create(req.body)
+    return {message: "Data saved"}
   } catch (error) {
     console.log(error)
     res.sendStatus(500)
