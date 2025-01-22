7446e13d5b754c7a4d2439bce71c1723727755fd
1Omon <1serwise@gmail.com>
2025-01-22 03:41:28 +0000

    added file uploadstorage route

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -24,7 +24,18 @@ const server = http.createServer(app);
 server.listen(process.env.PORT, ()=>{
     console.log("server running on port 8000")
 })
+app.post('/api/upload-files', async (req, res) => {
+    const { fileUrls } = req.body; // Expect an array of URLs
+    try {
+      // Save fileUrls to your database or perform other actions
+        console.log("received")
+      res.status(200).json({ message: "Files uploaded successfully!" });
+    } catch (error) {
+      res.status(500).json({ error: "Failed to save file URLs." });
+    }
+  });
 
+  
 app.use("/api/applicants", router);
 mongoose.Promise = Promise
 mongoose.connect(process.env.DB_URL)
